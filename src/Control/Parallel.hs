1d880ae70bb68ea8f8f2336eb854a4a57c95eb17
Connor James <connor@fossa.com>
2019-10-21 12:53:03 -0700

    WIP

--- src/Control/Parallel.hs
+++ src/Control/Parallel.hs
@@ -10,10 +10,20 @@ import Polysemy
 import Polysemy.Async
 import Polysemy.Resource
 
-data State action = State
-  { stQueued    :: TVar [action]
-  , stRunning   :: TVar Int
-  , stCompleted :: TVar Int
+newtype ActionId = ActionId { unActionId :: Int }
+  deriving (Eq, Ord, Show, Generic)
+
+data Action ty = Action
+  { actionId     :: Int
+  , actionAction :: ty
+  , actionDeps   :: [ActionId]
+  }
+
+data State ty = State
+  { stActionIdCur :: TVar Int
+  , stQueued      :: TVar [Action ty]
+  , stRunning     :: TVar [ActionId]
+  , stCompleted   :: TVar Int
   }
 
 data Progress = Progress
@@ -35,18 +45,28 @@ runActions :: forall r action
             . Members '[Embed IO, Async, Resource] r
            => Int -- number of threads
            -> [action] -- initial actions
-           -> ((action -> Sem r ()) -> action -> Sem r ()) -- given an action to enqueue more actions, run an action
+           -> ((action -> [ActionId] -> Sem r ActionId) -> action -> Sem r ()) -- given an action to enqueue more actions, run an action
            -> (Progress -> Sem r ()) -- get progress updates
            -> Sem r ()
 runActions numThreads initial runAction reportProgress = do
   state <- embed $
-    State <$> newTVarIO initial
+    State <$> newTVarIO 0
+          <*> newTVarIO []
+          <*> newTVarIO []
           <*> newTVarIO 0
-          <*> newTVarIO 0
+
+  let enqueue :: Member (Embed IO) r => action -> [ActionId] -> Sem r ActionId
+      enqueue action deps = embed $ atomically $ do
+        actionId <- readTVar (stActionIdCur state)
+        writeTVar (stActionIdCur state) (actionId + 1)
+
+        modifyTVar (stQueued state) (Action actionId action deps :)
+        pure (ActionId actionId)
+
+  -- Enqueue initial actions
+  traverse_ (\act -> enqueue act []) initial
 
   _ <- async $ updateProgress reportProgress state
-
-  let enqueue action = embed $ atomically $ modifyTVar (stQueued state) (action:)
 
   if numThreads > 1
     then do
@@ -57,7 +77,7 @@ runActions numThreads initial runAction reportProgress = do
         queued  <- readTVar (stQueued state)
         check (null queued)
         running <- readTVar (stRunning state)
-        check (running == 0)
+        check (null running)
 
     else worker (runAction enqueue) state
 
@@ -71,7 +91,7 @@ updateProgress f st@State{..} = loop (Progress 0 0 0)
     queued <- length <$> readTVar stQueued
     completed <- readTVar stCompleted
 
-    let new = Progress running queued completed
+    let new = Progress (length running) queued completed
 
     check (prev /= new)
 
@@ -83,7 +103,7 @@ stopWhenDone State{..} act = do
   case queued of
     [] -> do
       running <- readTVar stRunning
-      if running == 0
+      if null running
         then pure (pure ())
         else act
     _ -> act
@@ -93,16 +113,20 @@ worker runAction st@State{..} = loop
   where
 
   loop = join $ embed $ atomically $ stopWhenDone st $ do
+    running <- readTVar stRunning
     queued <- readTVar stQueued
     case queued of
       [] -> retry
       (x:xs) -> do
         writeTVar stQueued xs
         addRunning
-        pure $ runAction x `finally` (complete *> loop)
+        --pure $ runAction x `finally` (complete *> loop)
+        undefined
 
   addRunning :: STM ()
-  addRunning = modifyTVar stRunning (+1)
+  --addRunning = modifyTVar stRunning (+1)
+  addRunning = undefined
 
   complete :: Sem r ()
-  complete = embed $ atomically $ modifyTVar stRunning (subtract 1) *> modifyTVar stCompleted (+1)
+  complete = undefined
+  --complete = embed $ atomically $ modifyTVar stRunning (subtract 1) *> modifyTVar stCompleted (+1)
